dc58ca40e0def5c00bc9e4ab507d93771579806b
bootstrap_tutorials <abeecool1480@gmail.com>
2024-08-29 22:27:37 -0700

    adding client and server in same repo

---
+++ server/dist/js/about.8ff89835.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkclient"]=self["webpackChunkclient"]||[]).push([[594],{4772:function(n,u,e){e.r(u),e.d(u,{default:function(){return f}});var t=function(){var n=this,u=n._self._c;return u("h1",[n._v("About Page")])},l=[],c=e(1656),r={},s=(0,c.A)(r,t,l,!1,null,null,null),f=s.exports}}]);
+//# sourceMappingURL=about.8ff89835.js.map
